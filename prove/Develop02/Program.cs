b835f566873e6f4272278fedb3ae39f75afe2c9f
luyinga <lumbaleluyinga2018@gmail.com>
2023-02-25 12:28:25 +0200

    things Added

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,43 @@
-using System;
+using system;
+using System.IO;
+
 
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Develop02 World!");
+    
+    {   
+        string journal = "myJournal";
+        string[] lines = System.IO.File.ReadAllLines(journal);
+
+        foreach (string line in lines)
+        Console .WriteLine(line);
+    }
+ class Date 
+
+ {
+     // Get the current date.
+     DateTime theCurrentTime = DateTime.Now;
+     string dateText = theCurrentTime.ToShortDateString();
+      Console.WriteLine(thisDay.ToString("d"));
+ }
+
+
+
+        class welcome
+
+        {   string greeting = "Welcme to the journal program ?";
+            string select ="Please select one of the choices:"
+            Console.WriteLine($"{greeting}");
+            Console.WriteLine($"{select}");
+
+            string word1 = "Write";
+            string word2 = "Display";
+            string word3 = "Load";
+            string word4 = "save";
+            string word5 = "Quit";
+            Console.WriteLine("What would you like to do?");
+        }
+    
     }
 }
